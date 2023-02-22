aa2f23338041e011396cf59f77d8983bdb1cb857
Lailatul Badriyah <lailatulbadriyah654@gmail.com>
2023-02-22 14:11:01 +0700

    Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM: 2141720036 || Nama: Lailatul Badriyah';
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
 });
