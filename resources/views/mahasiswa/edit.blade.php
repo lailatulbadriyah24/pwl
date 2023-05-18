3ef1680a20495052cc393a25f13bcb42198f03b9
lailatulbadriyah24 <lailatulbadriyah654@gmail.com>
2023-05-18 21:37:29 +0700

    [UPDATE] Pertemuan 10 Tugas 2

--- resources/views/mahasiswa/edit.blade.php
+++ resources/views/mahasiswa/edit.blade.php
@@ -65,7 +65,7 @@
                             <label for="image">Foto</label>
                             <input type="file" class="form-control" required="required" name="image"
                                 value="{{ $mahasiswa->foto }}"></br>
-                            <img width="100px" height="100px" src="{{ asset('storage/' . $mahasiswa->foto) }}">
+                            <img width="100px" src="{{ asset('storage/' . $mahasiswa->foto) }}">
                         </div>
                         <div class="form-group">
                             <label>Jenis Kelamin</label>
